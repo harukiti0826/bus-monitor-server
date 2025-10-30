ffc7b0875cdd3c6f39cece0da8251ded964b9b83
Tada Haruki <172946533+harukiti0826@users.noreply.github.com>
2025-10-30 18:30:29 +0900

    Update server.py

--- server.py
+++ server.py
@@ -342,6 +342,7 @@ def push():
     return jsonify({"ok": True})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 500))
     app.run(host="0.0.0.0", port=port)
+
 
